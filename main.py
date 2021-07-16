d297e012e1c0872d784be71d7cf18d7580b527af
Crist128 <cristoballongares@gmail.com>
2021-07-15 22:47:31 -0500

    que

---
+++ main.py
@@ -0,0 +1,9 @@
+n1 = int(input("Ingresa el valor: "))
+
+
+if n1 % 2 == 0:
+	print(f'El valor es numero par')
+else:
+	print(f'El valor no es numero par')
+
+	#quequeueuquequeekwyuXdasdhjagjasdhahelpqSSaaaaaaa
