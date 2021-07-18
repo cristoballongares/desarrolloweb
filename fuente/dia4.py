5b7890325b2a74c1ee7abd56cd319259a6e343a2
Crist128 <cristoballongares@gmail.com>
2021-07-18 11:01:06 -0500

    Lol, no se como logre solucionar esto, pero creo voy bien :)

---
+++ fuente/dia4.py
@@ -0,0 +1,10 @@
+print("Daras 2 numeros, y se sabra que numero es mas mayor de los 2\n")
+
+
+n1 = int(input("Escribe el valor del primer numero: "))
+n2 = int(input("Escribe el valor del segundo numero: "))
+
+if n1 > n2:
+	print(f"{n1} es mayor")
+else:
+	print(f"{n2} es mayor")
