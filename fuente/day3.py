ca63f8f74b1af295d6b93d56c09dd0126eb151c8
Crist128 <cristoballongares@gmail.com>
2021-07-17 22:14:24 -0500

    ADD dia3v2

--- fuente/day3.py
+++ fuente/day3.py
@@ -1,9 +1,11 @@
+# Ejercicio para ver si un numero esta dentro de un rango :)
+
 n1 = int(input('Ingresa el valor uno: '))
 
 valorminimo = 0
 valormaximo = 5
 
-estaenrango = (valorminimo <= n1) and (valormaximo >= n1)
+estaenrango = (valorminimo <= n1) and (valormaximo >= n1) # Expresiones Booleanas
 
 if estaenrango:
 	print(f'El numero {n1} esta dentro de rango')
---
+++ fuente/dia3v2.py
@@ -0,0 +1,12 @@
+# Ejercisio para saber si puedo asistir a ver un juego si tengo dia de descanso o vaciones
+
+diadedescanso = False
+dialibre = False
+
+# Si con una sola que sea verdadera, devolvera que si puede, 
+#pero si las 2 son, siginifica que no puede asistir
+
+if diadedescanso or dialibre: 
+	print("Si puede asisitir")
+else:
+	print("No puede asisitir")
