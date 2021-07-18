e0359c467f13e55807cceb3112dbd74eea243383
Crist128 <cristoballongares@gmail.com>
2021-07-17 22:47:31 -0500

    Ejercicio 10/10 | Dia 3 Ejercicio Final

--- fuente/dia3finally.py
+++ fuente/dia3finally.py
@@ -1,18 +1,21 @@
-n1 = int(input("Digita tu edad: "))
+n1 = int(input("Digita tu edad: ")) # supongo que esto no tiene explicacion.
 
-veintes = n1 >= 20 and n1 < 30
+veintes = n1 >= 20 and n1 < 30 # verificamos si esta en el rango 20
 #print(veintes)
 
-treintas = (n1 >= 30) and (n1 < 40)
+treintas = (n1 >= 30) and (n1 < 40) # verificamos si esta en el rango 30
 #print(treintas)
 
-if veintes or treintas:
-	print("Estas en el rango de los 20-30")
+
+# Comienza lo bueno diria yo lmao
+
+if veintes or treintas: 
+	print("Estas en el rango de los 20-30") # Devolvera esto por si esta en los 2 rangos, de lo contrario...
 
 	if veintes:
-		print("Estas en los 20`s")
+		print("Estas en los 20`s") # Esto devolvera si esta en los 20`s
 	elif treintas:
-		print("Estas en los 30`s")
+		print("Estas en los 30`s") # Esto devolvera si esta en los 30`s
 
 else:
-	print("No estas en el rango ni en 30 ni en 20")
+	print("No estas en el rango ni en 30 ni en 20") # Si no esta en ningun rango, devolvera esto
